250b26e2638f84b320b1fd84740606ff6bff38dd
Mohammad JAQMAQJI <mohjak@gmail.com>
2016-03-01 03:28:49 +0200

    Edit and Create Post with tests.

---
+++ MVCCMS/Data/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using MVCCMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCCMS.Data
+{
+	public interface IPostRepository
+	{
+		Post Get(string id);
+		void Edit(string id, Post udatedItem);
+		void Create(Post model);
+		IEnumerable<Post> GetAll();
+	}
+}
